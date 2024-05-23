e2c2f11bc561a9a3a6a70d4c6bbd335816eaccf1
Rameshwara Yellepeddi <rameshwara.yellepeddi@cognine.com>
2024-05-23 20:56:25 +0530

    Auth Web API

---
+++ AuthenticationAndAuthorizationAssignment/AuthenticationAndAuthorizationAssignment/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AuthenticationAndAuthorizationAssignment.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationAndAuthorizationAssignment.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
